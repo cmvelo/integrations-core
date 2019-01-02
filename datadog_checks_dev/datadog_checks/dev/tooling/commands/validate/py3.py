3872e22494f6b5991dc53b74616fe184abdcc172
Hippolyte HENRY <hippolyte.henry@datadoghq.com>
2019-01-02 19:03:01 +0100

    Use standalone py3 validation

--- datadog_checks_dev/datadog_checks/dev/tooling/commands/validate/py3.py
+++ datadog_checks_dev/datadog_checks/dev/tooling/commands/validate/py3.py
@@ -2,17 +2,14 @@
 # All rights reserved
 # Licensed under a 3-clause BSD style license (see LICENSE)
 import click
-import json
 import os
-from contextlib import closing
-from operator import itemgetter
 try:
     from StringIO import StringIO
 except ImportError:
     from io import StringIO
 
-from pylint.lint import PyLinter, fix_import_path
-from pylint.reporters.json import JSONReporter
+from a7 import validate_py3
+from six import iteritems
 
 from ..console import CONTEXT_SETTINGS, abort, echo_failure, echo_info, echo_success
 from ...constants import get_root, NOT_CHECKS
@@ -41,39 +38,14 @@ def py3(check):
         abort("{} does not exist.".format(path_to_module))
 
     echo_info("Validating python3 compatibility of {}...".format(check))
-    with closing(StringIO()) as out:
-        linter = PyLinter(reporter=JSONReporter(output=out))
-        linter.load_default_plugins()
-        linter.python3_porting_mode()
-        # Disable `no-absolute-import`, which checks for a behaviour that's already part of python 2.7
-        # cf https://www.python.org/dev/peps/pep-0328/
-        linter.disable("no-absolute-import")
-        with fix_import_path([path_to_module]):
-            linter.check(path_to_module)
-            linter.generate_reports()
-        results = json.loads(out.getvalue() or "{}")
+    results = validate_py3(path_to_module)
 
     if results:
         echo_failure("Incompatibilities were found for {}:".format(check))
-        current_path = None
-        for problem in sorted(results, key=itemgetter("path")):
-            # An issue found by pylint is a dict like
-            # {
-            #     "message": "Calling a dict.iter*() method",
-            #     "obj": "OpenFilesCheck.check",
-            #     "column": 27,
-            #     "path": "/path/to/file.py",
-            #     "line": 235,
-            #     "message-id": "W1620",
-            #     "type": "warning",
-            #     "symbol": "dict-iter-method",
-            #     "module": "file"
-            # }
-            path = problem["path"]
-            if current_path is None or path != current_path:
-                echo_info("File {}:".format(path))
-            echo_failure("  Line {}, column {}: {}".format(problem["line"], problem["column"], problem["message"]))
-            current_path = path
+        for file, problems in iteritems(results):
+            echo_info("File {}:".format(file))
+            for problem in problems:
+                echo_failure("  {}".format(problem))
         abort()
     else:
         echo_success("{} is compatible with python3".format(check))
